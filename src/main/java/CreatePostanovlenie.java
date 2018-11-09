ef27b7d13ad4e5c249afdf0ad8421664ae6547b0
tkgreg <tkachuk.g@gmail.com>
2018-11-09 11:25:44 +0100

    Exception handler added, try-catch refactored

--- src/main/java/CreatePostanovlenie.java
+++ src/main/java/CreatePostanovlenie.java
@@ -48,45 +48,39 @@ public class CreatePostanovlenie {
 //        Проводим работу последовательно по отделам по карте
         for (List<BeanConnect> dataSource : dataSources.values()) {
             //        Состовляем имя процесса
-//            String ProcessName = properties.get("ProcessName") + "_Postanovlenie";
             ProcessName = dataSource.get(0).getProcessName() + "_Postanovlenie";
 
-            System.out.println(ProcessName+" Обрабатываем БД "+dataSource.get(0).getBeanId());
+            System.out.println(ProcessName + " Обрабатываем БД " + dataSource.get(0).getBeanId());
             MyLogger.get().logMessage(ProcessName, " Обрабатываем БД " + dataSource.get(0).getBeanId());
             Logger.getLogger(CreatePostanovlenie.class).info(" Обрабатываем БД " + dataSource.get(0).getBeanId());
 
-            PostService service = null;
-
+            PostService service ;
 
             try {
                 service = new PostService(dataSource, properties, dd, ProcessName);
 //            создание постановлений
                 service.processCreate();
-
-
             } catch (Exception e) {
-//                e.printStackTrace();
-                System.out.println(ProcessName+" Исключение в главном классе "+e.getMessage()+" "+e);
-                MyLogger.get().logMessage(ProcessName, "Исключение в главном классе "+e.getMessage());
-                Logger.getLogger(CreatePostanovlenie.class).error("Исключение в главном классе "+e.getMessage());
+                exceptionHandler(e);
+                System.out.println(ProcessName + " Исключение в главном классе " + e.getMessage() + " " + e);
+                MyLogger.get().logMessage(ProcessName, "Исключение в главном классе " + e.getMessage());
+                Logger.getLogger(CreatePostanovlenie.class).error("Исключение в главном классе " + e.getMessage());
             }
         }
 
-        boolean zipFiles = false;
+        boolean zipFiles;
         File[] files = new File(properties.get("OUTPUT_DIRECTORY")).listFiles(new FilenameFilter() {
             @Override
             public boolean accept(File file, String s) {
-//                System.out.println(s);
-//                return s.toLowerCase().matches("^piev_[0-9]{14}.xml.zip");
                 return s.toLowerCase().matches("^piev_[0-9]{14}.xml.zip");
 
             }
         });
         Logger.getLogger(CreatePostanovlenie.class).info("Количество файлов для архивации: " + files.length);
         MyLogger.get().logMessage(ProcessName, "Количество файлов для архивации: " + files.length);
-                zipFiles = zipAddAll(files);
-        if (zipFiles != false){
-            for (File filenam : files){
+        zipFiles = zipAddAll(files);
+        if (zipFiles) {
+            for (File filenam : files) {
                 filenam.delete();
             }
         }
@@ -96,26 +90,22 @@ public class CreatePostanovlenie {
     //    Пакуем пиев документы в один архив.
     private boolean zipAddAll(File[] files) {
 
-//        AddZip(String path, String outFilename) {
-//            File[] filenames = new File(path).listFiles();
         Calendar inst = Calendar.getInstance();
         String day = new DecimalFormat("00").format(inst.get(Calendar.DAY_OF_MONTH));
         String month = Integer.toHexString(inst.get(Calendar.MONTH) + 1);
 
         byte[] buf = new byte[1024];
 
+        ZipOutputStream out = null;
         try {
-//            String outFilename = "outfile.zip";
             String outFilename = "p" + properties.get("KOD_OSP") + "00" + day + month + "." + "1ss";
-            ZipOutputStream out = new ZipOutputStream(new FileOutputStream(properties.get("OUTPUT_DIRECTORY")+outFilename));
+            out = new ZipOutputStream(new FileOutputStream(properties.get("OUTPUT_DIRECTORY") + outFilename));
 
-            // Compress the files
-//                for (File filename : filenames) {
             for (File filename : files) {
-
+                FileInputStream in = null;
                 try {
                     if (!filename.isFile()) continue;
-                    FileInputStream in = new FileInputStream(filename);
+                    in = new FileInputStream(filename);
 
                     //здесь ТОЛЬКО ИМЯ!
                     out.putNextEntry(new ZipEntry(filename.getName()));
@@ -128,25 +118,42 @@ public class CreatePostanovlenie {
 
                     // Complete the entry
                     out.closeEntry();
-                    in.close();
                 } catch (IOException e) {
+                    exceptionHandler(e);
                     System.err.println("Не удалось запаковать файл: " + filename.getAbsolutePath());
                     Logger.getLogger(CreatePostanovlenie.class).error("Не удалось запаковать файл: " + filename.getAbsolutePath());
                     MyLogger.get().logMessage(ProcessName, "Не удалось запаковать файл: " + filename.getAbsolutePath());
                     return false;
+                } finally {
+                    tryToCloseResource(in);
                 }
             }
 
             // Complete the ZIP file
-            out.close();
             Logger.getLogger(CreatePostanovlenie.class).info("Подготовлен архив: " + outFilename);
             MyLogger.get().logMessage(ProcessName, "Подготовлен архив: " + outFilename);
         } catch (IOException e) {
             Logger.getLogger(CreatePostanovlenie.class).error("Ошибка при формировании архива: " + e.getMessage());
             MyLogger.get().logMessage(ProcessName, "Ошибка при формировании архива: " + e.getMessage());
             return false;
+        } finally {
+            tryToCloseResource(out);
         }
         return true;
+    }
+
+    private static void tryToCloseResource(Closeable closeable) {
+        try {
+            if (closeable != null) {
+                closeable.close();
+            }
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+
+    private static void exceptionHandler(Exception ex) {
+        ex.printStackTrace(); // should be replaced with proper logger
     }
 
     private LinkedHashMap<String, String> getProperties() {
